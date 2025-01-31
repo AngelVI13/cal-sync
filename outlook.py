c012ea31d07d1ea24566693f91871b6c33ec25a9
AngelVI <a.v.iliev13@gmail.com>
2025-01-31 09:49:13 +0200

    handle edge cases

--- outlook.py
+++ outlook.py
@@ -1,7 +1,10 @@
+import json
 import time
 import unittest
 import subprocess
-from typing import Set
+import configparser
+from pathlib import Path
+from typing import Set, Dict
 from pprint import pprint
 from dataclasses import dataclass
 from ipdb import set_trace
@@ -19,25 +22,50 @@ class MeetingInfo:
     def __hash__(self) -> int:
         return hash((self.from_, self.when, self.sent_time))
 
-
-class SimpleCalculatorTests(unittest.TestCase):
-    run_driver = False
+    def to_dict(self) -> Dict[str, str]:
+        return self.__dict__
 
     @classmethod
+    def from_dict(cls, d: Dict[str, str]):
+        return cls(from_=d["from_"], when=d["when"], sent_time=d["sent_time"])
 
+
+class SimpleCalculatorTests(unittest.TestCase):
+    config_path = Path("config.ini")
+
+    @classmethod
     def setUpClass(self):
-        # TODO: update this script to automatically start the winappdriver process!!
-        #set up appium
+        if not (self.config_path.exists() and self.config_path.is_file()):
+            raise Exception(f"Failed to find config file: {self.config_path.absolute().as_posix()}")
+
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_path)
+
+        settings = self.config["settings"]
+        self.run_driver = settings.getboolean("RunWinAppDriver")
+        self.meeting_folder_name = settings["MeetingsFolderName"]
+        self.forward_address = settings["ForwardAddress"]
+
+        self.history_file = Path(settings["HistoryFile"])
+        self.already_sent: Dict[int, MeetingInfo] = {}
+        if self.history_file.exists() and self.history_file.is_file():
+            s = self.history_file.read_text(encoding="utf-8")
+            data = json.loads(s)
+            self.already_sent = {} 
+            for info in data:
+                d = MeetingInfo.from_dict(info)
+                self.already_sent[hash(d)] = d
+
         if self.run_driver:
             self.win_app_driver = subprocess.Popen(
-                args=[r"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe"],
+                args=[settings["WinAppDriverExePath"]],
                 shell=True,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
             )
 
         desired_caps = {}
-        desired_caps["app"] = r"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+        desired_caps["app"] = settings["OutlookExePath"]
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723',
             desired_capabilities= desired_caps)
@@ -50,7 +78,7 @@ class SimpleCalculatorTests(unittest.TestCase):
             self.win_app_driver.terminate()
 
     def openMeetingRequests(self):
-        meetingRequestsFolder = self.driver.find_element_by_name("Meeting Requests")
+        meetingRequestsFolder = self.driver.find_element_by_name(self.meeting_folder_name)
         meetingRequestsFolder.click()
         time.sleep(1)
 
@@ -76,37 +104,43 @@ class SimpleCalculatorTests(unittest.TestCase):
         else:
             # a popup is shown -> accept it
             self.driver.find_element_by_name("OK").click()
-            time.sleep(0.5)
+            time.sleep(1)
 
     def forwardMail(self):
         # forward email
         msg_actions = self.driver.find_element_by_name("Message Actions")
         other_actions = msg_actions.find_element_by_name("Other Actions")
         other_actions.click()
-        time.sleep(0.5)
+        time.sleep(1)
 
         prev_handles = set(self.driver.window_handles)
         main_window = self.driver.current_window_handle
 
         self.driver.find_element_by_name("Forward").click()
-        time.sleep(0.5)
+        time.sleep(1)
 
         self.handleSeriesPopup(prev_handles)
 
         new_handles = set(self.driver.window_handles)
-        mail_forward, *_ = new_handles - prev_handles
+        handle_diff = new_handles - prev_handles
+        if len(handle_diff) == 0:
+            # In cases where "Forward" button is disabled - no new window openes
+            return False
+
+        mail_forward, *_ = handle_diff
         self.driver.switch_to.window(mail_forward)
 
-        to_box = self.driver.find_element_by_name("RichEdit20WPT")
-        to_box.send_keys("angel.iliev@qdevtechnologies.com")
+        to_box = self.driver.find_element_by_class_name("RichEdit20WPT")
+        to_box.send_keys(self.forward_address)
 
         self.driver.find_element_by_name("Send").click()
         self.driver.switch_to.window(main_window)
+        return True
 
     def mailInfo(self, m) -> MeetingInfo:
         m.click()
 
-        time.sleep(0.5)  # wait for elements to load
+        time.sleep(1)  # wait for elements to load
 
         when = self.driver.find_elements_by_class_name("Change Highlighting Edit Control")[0].text
         from_ = self.driver.find_element_by_name("From").text
@@ -114,6 +148,10 @@ class SimpleCalculatorTests(unittest.TestCase):
 
         new_info = MeetingInfo(from_=from_, when=when, sent_time=sent)
         return new_info
+
+    def syncToFile(self, info: Dict[int, MeetingInfo]):
+        data = [v.to_dict() for v in info.values()]
+        self.history_file.write_text(json.dumps(data), encoding="utf-8")
 
 
     def test_initialize(self):
@@ -135,16 +173,18 @@ class SimpleCalculatorTests(unittest.TestCase):
 
         self.openMeetingRequests()
 
-        # TODO: figure out how to click all of them (including scrolling down)
-        # because this just finds all visible elements
         email_box = self.driver.find_element_by_name("Table View")
-        down_btn = email_box.find_element_by_name("Line down")
+        try:
+            down_btn = email_box.find_element_by_name("Line down")
+        except:
+            # if we can't find the down button it means that there is not a lot of
+            # emails -> no need to scroll down
+            done = True
+        else:
+            done = False
 
-        info = []
-        done = False
+        info = self.already_sent
         for _ in range(20):
-            # TODO: can't use the set directly cause the elements are based on position so when you scrol
-            # the same email is considered a completely different element
             meetings = email_box.find_elements_by_class_name("LeafRow")
             # set_trace()
 
@@ -156,7 +196,7 @@ class SimpleCalculatorTests(unittest.TestCase):
                 processed += 1
 
                 new_info = self.mailInfo(m)
-                if new_info in set(info):
+                if hash(new_info) in info:
                     # the first time you encounter a duplicate -> flag to stop
                     # but keep going to make sure all potential meetings are clicked
                     # This solves the issue when you cant scroll to a full set of 
@@ -165,9 +205,13 @@ class SimpleCalculatorTests(unittest.TestCase):
                     done = True
                     continue
 
-                self.forwardMail()
-                info.append(new_info)
-                set_trace()
+                success = self.forwardMail()
+                if not success:
+                    print(f"Failed to forward email (forwarding disable): {new_info}")
+
+                info[hash(new_info)] = new_info
+                self.syncToFile(info)
+                # set_trace()
 
             if done:
                 break
